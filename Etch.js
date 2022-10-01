6b61cd8215a7e469f80a088543dd5d5dbca349a7
Tyler Sanichara <Tsanichara67@gmail.com>
2022-10-01 14:35:57 -0400

    Function to create grid now takes in a number that determines amount of squares per side in the grid

--- Etch.js
+++ Etch.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
-
+/*
 function createDivs(){
 
     for(let i = 0; i < 256; i++){
@@ -14,8 +14,28 @@ function createDivs(){
     }
 
 }
+*/
 
-createDivs();
+
+function createDivs(n){
+    let square = n**2;
+
+    for(let i = 0; i < square; i++){
+        let newDiv = document.createElement("div");
+        newDiv.classList.add('box');
+        newDiv.style.outline = "1px solid black";
+        newDiv.addEventListener("mouseover", function(){
+            newDiv.style.backgroundColor = "black";
+        })
+
+        container.appendChild(newDiv);
+    }
+
+    container.style.gridTemplateRows = `repeat(${n}, auto)`;
+    container.style.gridTemplateColumns = `repeat(${n}, auto)`;
+}
+
+createDivs(16);
 
 
 
